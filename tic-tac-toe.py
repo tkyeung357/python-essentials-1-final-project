77dc4ef1d514e5da69aa92e1e5f73914eea37ca8
Tim Yeung <timyeung357@me.com>
2023-05-17 21:14:15 -0700

    done, i will back to enhance this program when i finish python essential 2

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -13,105 +13,185 @@
 
 # The example session with the program may look as follows:
 from random import randrange
+import sys
 
 def display_board(board):
-    # The function accepts one parameter containing the board's current status
-    # and prints it out to the console.
-    for row in board:
-        for cols in row:
-            print(cols)
+	# The function accepts one parameter containing the board's current status
+	# and prints it out to the console.
+	for row in board:
+		print("\n")
+		for cols in row:
+			print(" | ", cols, end="")
+		print("\n")
+		print("-------------------")
 
 # updating board status
 # @param board list
 # @param move tuple
 # @param sign string default X for computer turn 
 def update_board(board, move, sign = 'X'):
-    try:
-        # update board status 
-        board[move[0]][move[1]] = sign
-        return board
-    except TypeError:
-        print('invaild type found, board should be a list, move should be a tuple')
-    except:
-        print('something wrong when updating board')
+	try:
+		# update board status 
+		board[move[0]][move[1]] = sign
+		return board
+	except TypeError:
+		print('invaild type found, board should be a list, move should be a tuple')
+	except:
+		print('something wrong when updating board')
 
 
 # move conver tto field sig with tuple of (row, col)
-def move_to_field_sign(move):
-    row = move // 3
-    col = move % 3
-    return (row, col)
+def move_to_field(move):
+	move -= 1
+	# row , col,
+	return (move // 3, move % 3)
 
 #check available field user choose 
 def is_move_in_available_field(board, move, available_fields):
-    is_available = False
-    field_sign = move_to_field_sign(move)
+	field = move_to_field(move)
 
-    if (len(available_fields) > 0):
-        for field in available_fields:
-            if field_sign == field:
-                is_available = True
-
-    return is_available
+	return len(available_fields) and field in available_fields
 
 
 #check move 
 def is_valid_move(move):
-    return move > 0 and move < 9
+	return move > 0 and move < 10
 
 def enter_move(board):
-    # The function accepts the board's current status, asks the user about their move, 
-    # checks the input, and updates the board according to the user's decision.
-    available_fields = make_list_of_free_fields(board)
-    while True:
-        try:
-            move = int(input("Player, please enter your move: "))
+	# The function accepts the board's current status, asks the user about their move, 
+	# checks the input, and updates the board according to the user's decision.
+	available_fields = make_list_of_free_fields(board)
+	while True:
+		try:
+			display_board(board)
+			move = int(input("Player, please enter your move: "))
 
-            if(not is_valid_move(move)):
-                print("move not in acceptable range 1-9, please try again")
-                continue
+			if(not is_valid_move(move)):
+				print("move not in acceptable range 1-9, please try again")
+				continue
 
-            # check available field 
-            if (not is_move_in_available_field(board, move, available_fields)):
-                print("field already been occupied, please try again")
-                continue
+			move_field = move_to_field(move)
+			# check available field 
+			if (move_field not in available_fields):
+				print("field already been occupied, please try again")
+				continue
 
-            # update board 
-            board = update_board(board, move_to_field_sign(move), 'O')
-            # check victory
-            # compute turn
-            draw_move(board)
-            # check victory
-            # update available fields
-            available_fields = make_list_of_free_fields(board)
-        except TypeError:
-            print("a invaild input found, please try again")
-        except:
-            print("something wrong...")
+			# update board 
+			board = update_board(board, move_field, 'O')
+			# board check
+			if (victory_for(board)):
+				break;
+		
+			# compute turn
+			move_field = draw_move(board)
+			board = update_board(board, move_field)
+			# board check
+			if (victory_for(board)):
+				break;
+			# update available fields
+			available_fields = make_list_of_free_fields(board)
+		except TypeError as e:
+			print("a invaild input found, please try again")
+		except Exception as e:
+			print("something wrong...")
 
 
 def make_list_of_free_fields(board):
-    # The function browses the board and builds a list of all the free squares; 
-    # the list consists of tuples, while each tuple is a pair of row and column numbers.
-    available_fields = []
-    for row in range(len(board)):
-        for col in range(len(board[row])):
-            current_field = board[row][col]
-            if type(current_field) is int:
-                available_fields.append(current_field)
-    
-    return available_fields            
+	# The function browses the board and builds a list of all the free squares; 
+	# the list consists of tuples, while each tuple is a pair of row and column numbers.
+	available_fields = []
+	for row in range(len(board)):
+		for col in range(len(board[row])):
+			current_field = board[row][col]
+			if type(current_field) is int:
+				available_fields.append((row, col))
+	
+	return available_fields            
 
+# return with list of tuples
+def get_list_of_horizontal(board):
+	list = []
+	for ir, row in enumerate(board):
+		horizon_list = []
+		for ic, col in enumerate(row):
+			horizon_list.append(board[ir][ic])
+		list.append(tuple(horizon_list))
+	return list
 
-def victory_for(board, sign):
-    # The function analyzes the board's status in order to check if 
-    # the player using 'O's or 'X's has won the game
-    pass
+# return with list of tuples
+def get_list_of_vertical(board):
+	list = []
+	# go though vertical 
+	for ic in range(len(board[0])):
+		#init vertical list
+		vertical_list = []
+		for ir, row in enumerate(board):
+			vertical_list.append(board[ir][ic])
+		list.append(tuple(vertical_list)) 
+	return list
 
+def get_list_of_diagonal(board):
+	list = []
+	list += get_diagonal_from_board_left_corner(board=board)
+	#reverse board and get another diagonal
+	reversed_board = get_reversed_board(board)
+	list += get_diagonal_from_board_left_corner(reversed_board)
+	return list
+
+# return with list of tuples
+def get_diagonal_from_board_left_corner(board):
+	list = []
+	diagonal_list = []
+	for i, row in enumerate(board):
+		diagonal_list.append(board[i][i])
+	list.append(tuple(diagonal_list))
+	return list
+
+# reverse the board 
+def get_reversed_board(board):
+	reversed_board = []
+	for row in board:
+		# copy row to prevent board got modified
+		tmp = row[:]
+		tmp.reverse()
+		reversed_board.append(tmp)
+	return reversed_board
+
+def victory_for(board):
+	# The function analyzes the board's status in order to check if 
+	# the player using 'O's or 'X's has won the game
+	horizontal_list = get_list_of_horizontal(board)
+	vertical_list =	get_list_of_vertical(board)
+	diagonal_list = get_list_of_diagonal(board)
+
+	all_list = horizontal_list + vertical_list + diagonal_list
+
+	winner_sign = None
+	for list in all_list:
+		if list[0] == list [1] == list [2]:
+			#we have a winner
+			winner_sign = list[0]
+	
+	if (type(winner_sign) is str): 
+		display_board(board)
+		if (winner_sign == 'O'):
+			print ("game over, victory is player ",  winner_sign)
+		else:
+			print ("game over, victory is computer ", winner_sign)
+
+		return True
+	return False
 
 def draw_move(board):
-    # The function draws the computer's move and updates the board.
-    available_fields = make_list_of_free_fields(board)
-    random_field = randrange(len(available_fields))
-    return update_board(board, available_fields[random_field])
+	# The function draws the computer's move and updates the board.
+	available_fields = make_list_of_free_fields(board)
+	random_field = randrange(len(available_fields))
+	return available_fields[random_field]
 
+board = [
+	[1, 2, 3],
+	[4, "X", 6],
+	[7, 8, 9]
+]
+
+enter_move(board)
